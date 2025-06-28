887f01d18f50d66e57a733ff930c4eb17e172519
hamkaysrgit config user.email hamkaysr12@gmail.com.gitgit config user.name hamkaysr <hamkasy12@gmail.com>
2025-06-28 14:29:52 +0700

    Initial commit - fitur auth Laravel

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100">
+
+    <div class="min-h-screen flex flex-col justify-center items-center px-4 py-12">
+        {{ $slot }}
+    </div>
+
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h2>Profil Pengguna</h2>
+<p>Nama: {{ $user->name }}</p>
+<p>Email: {{ $user->email }}</p>
